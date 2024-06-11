a443cdd400756dfcf1de4744531a4ff84da2e330
Ramkumar-1994 <nramkumar1994@gmail.com>
2024-06-11 13:12:11 +0530

    react weather app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div >
+      <div className='shadow-md'>
+      <h1 className='text-center p-3 border font-semibold text-2xl uppercase'>WeatherApp Api</h1>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
